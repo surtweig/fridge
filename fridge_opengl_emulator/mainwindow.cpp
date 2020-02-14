6364047b39d4b1005337253fb9f150c4cb61befb
surtweig <surtweig@gmail.com>
2020-02-15 00:06:58 +0200

    emulator: added RAM view

--- fridge_opengl_emulator/mainwindow.cpp
+++ fridge_opengl_emulator/mainwindow.cpp
@@ -8,8 +8,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     initFridge();
+
     ui->setupUi(this);
+    ramViewPosition = 0;
+    ramSelectedAddress = 0;
+
     updateRegistersView();
+    updateRAMView();
 }
 
 void MainWindow::initFridge()
@@ -53,14 +58,138 @@ void MainWindow::updateRegistersView()
     ui->cpuFlagSignCheckBox->setChecked(FRIDGE_cpu_flag_SIGN(sys->cpu));
 }
 
+void MainWindow::updateRAMView()
+{
+    int rowCount = RAMViewRowsCount;
+    int colCount = RAMViewColumnsCount;
+    ui->ramTableWidget->setRowCount(rowCount);
+    ui->ramTableWidget->setColumnCount(colCount);
+    int rowHeight = (ui->ramTableWidget->size().height() - ui->ramTableWidget->horizontalHeader()->height()) / rowCount - 1;
+    int colWidth = (ui->ramTableWidget->size().width() - ui->ramTableWidget->verticalHeader()->width()) / colCount - 1;
+
+    for (int i = 0; i < rowCount; ++i)
+        ui->ramTableWidget->setRowHeight(i, rowHeight);
+
+    for (int i = 0; i < colCount; ++i)
+        ui->ramTableWidget->setColumnWidth(i, colWidth);
+
+    for (int i = 0; i < colCount; ++i)
+    {
+        if (ui->ramTableWidget->horizontalHeaderItem(i) == nullptr)
+            ui->ramTableWidget->setHorizontalHeaderItem(i, new QTableWidgetItem());
+        ui->ramTableWidget->horizontalHeaderItem(i)->setText(QString::number(i, 16).toUpper());
+    }
+    for (int i = 0; i < rowCount; ++i)
+    {
+        if (ui->ramTableWidget->verticalHeaderItem(i) == nullptr)
+            ui->ramTableWidget->setVerticalHeaderItem(i, new QTableWidgetItem());
+        ui->ramTableWidget->verticalHeaderItem(i)->setText(QString::number(ramViewPosition + i*RAMViewColumnsCount, 16).toUpper());
+    }
+
+    for (int i = 0; i < rowCount; ++i)
+        for (int j = 0; j < colCount; ++j)
+        {
+            if (ui->ramTableWidget->item(i, j) == nullptr)
+                ui->ramTableWidget->setItem(i, j, new QTableWidgetItem());
+            ui->ramTableWidget->item(i, j)->setText(QString::number(sys->cpu->ram[ramViewPosition + i*colCount + j], 16).toUpper());
+        }
+
+    ui->ramTableWidget->clearSelection();
+    if (ramSelectedAddress >= ramViewPosition && ramSelectedAddress < ramViewPosition + rowCount*colCount)
+    {
+        int offset = ramSelectedAddress - ramViewPosition;
+        ui->ramTableWidget->item(offset / colCount, offset % colCount)->setSelected(true);
+    }
+
+    /*
+    int pc = sys->cpu->PC;
+    if (pc >= ramViewPosition && pc < ramViewPosition + rowCount*colCount)
+    {
+        int offset = pc - ramViewPosition;
+        int i = offset / colCount;
+        int j = offset % colCount;
+        QBrush brush = ui->ramTableWidget->item(i, j)->background();
+        brush.setColor(Qt::blue);
+        ui->ramTableWidget->item(i, j)->setBackground(brush);
+    }
+    */
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    updateRAMView();
+}
+
 void MainWindow::on_sysTickBtn_clicked()
 {
     FRIDGE_sys_tick(sys);
     updateRegistersView();
+    updateRAMView();
 }
 
 void MainWindow::on_sysResetBtn_clicked()
 {
     FRIDGE_cpu_reset(sys->cpu);
     updateRegistersView();
+    updateRAMView();
+}
+
+void MainWindow::on_ramScrollRightBtn_clicked()
+{
+    ramViewPosition += RAMViewRowsCount*RAMViewColumnsCount;
+    if (ramViewPosition >= FRIDGE_RAM_SIZE)
+        ramViewPosition = FRIDGE_RAM_SIZE - RAMViewRowsCount*RAMViewColumnsCount;
+    updateRAMView();
+}
+
+void MainWindow::on_ramScrollLeftBtn_clicked()
+{
+    ramViewPosition -= RAMViewRowsCount*RAMViewColumnsCount;
+    if (ramViewPosition < 0)
+        ramViewPosition = 0;
+    updateRAMView();
+}
+
+void MainWindow::on_ramAddrEdit_returnPressed()
+{
+    bool ok = false;
+    int addr = ui->ramAddrEdit->text().toInt(&ok, 16);
+    if (ok && addr >= 0 && addr < FRIDGE_RAM_SIZE)
+    {
+        ramSelectedAddress = addr;
+        ramViewPosition = (addr / (RAMViewRowsCount*RAMViewColumnsCount)) * RAMViewRowsCount*RAMViewColumnsCount;
+        updateRAMView();
+    }
+}
+
+void MainWindow::on_ramAddrEdit_textChanged(const QString &arg1)
+{
+    bool ok = false;
+    QString addrText = arg1;
+    int addr = addrText.toInt(&ok, 16);
+    QPalette pal = ui->ramAddrEdit->palette();
+    if ((ok && addr >= 0 && addr < FRIDGE_RAM_SIZE) || addrText.length() == 0)
+        pal.setColor(QPalette::Base, Qt::white);
+    else
+        pal.setColor(QPalette::Base, ErrorBackgroundColor);
+    ui->ramAddrEdit->setPalette(pal);
+}
+
+void MainWindow::on_ramScrollToPC_clicked()
+{
+    ramSelectedAddress = sys->cpu->PC;
+    ramViewPosition = (ramSelectedAddress / (RAMViewRowsCount*RAMViewColumnsCount)) * RAMViewRowsCount*RAMViewColumnsCount;
+    updateRAMView();
+}
+
+void MainWindow::on_ramScrollToSP_clicked()
+{
+    ramSelectedAddress = sys->cpu->SP;
+    ramViewPosition = (ramSelectedAddress / (RAMViewRowsCount*RAMViewColumnsCount)) * RAMViewRowsCount*RAMViewColumnsCount;
+    updateRAMView();
+}
+
+void MainWindow::on_asmCompileBtn_clicked()
+{
 }
--- fridge_opengl_emulator/mainwindow.h
+++ fridge_opengl_emulator/mainwindow.h
@@ -14,22 +14,42 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    const int RAMViewRowsCount = 16;
+    const int RAMViewColumnsCount = 16;
+    const QColor ErrorBackgroundColor = QColor::fromRgb(255, 128, 128);
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
     void updateRegistersView();
+    void updateRAMView();
 
 private slots:
     void on_sysTickBtn_clicked();
-
     void on_sysResetBtn_clicked();
+    void on_ramScrollRightBtn_clicked();
+    void on_ramScrollLeftBtn_clicked();
+    void on_ramAddrEdit_returnPressed();
+    void on_ramAddrEdit_textChanged(const QString &arg1);
+
+    void on_ramScrollToPC_clicked();
+
+    void on_ramScrollToSP_clicked();
+
+    void on_asmCompileBtn_clicked();
 
 private:
     Ui::MainWindow *ui;
     FRIDGE_SYSTEM *sys;
 
+    int ramViewPosition;
+    int ramSelectedAddress;
+
     void initFridge();
     void destroyFridge();
+
+protected:
+    void resizeEvent(QResizeEvent *event);
 };
 #endif // MAINWINDOW_H
